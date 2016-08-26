ca888367c3113768da83ea4eac251a014accb852
Daniel hernandez <danielhernandez16@hotmail.com>
2016-08-25 19:39:54 -0500

    Change url request in authServices

--- www/js/services/services-auth.js
+++ www/js/services/services-auth.js
@@ -2,10 +2,10 @@ var authServices = angular.module('authServices', ['ngResource']);
 
 var version = 'http://development.'
 var baseUrl = 'platxo-bi.appspot.com';
-var productsUrl = '/api/auth/';
+var authUrl = '/api/auth/';
 
 authServices.service('signupService', [ '$resource', function ($resource) {
-  return $resource(version + baseUrl + productsUrl +':id/?format=json', {id: '@id'},{
+  return $resource(version + baseUrl + authUrl +':id/?format=json', {id: '@id'},{
     create: { method: 'POST' },
     update: { method: 'PUT' }
   });
