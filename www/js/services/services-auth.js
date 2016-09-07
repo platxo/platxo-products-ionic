e4d7aa2557858a332af304fc4b23ca0e50c5e502
Daniel hernandez <danielhernandez16@hotmail.com>
2016-09-06 20:32:07 -0500

    Refactor url request with rootScope in app.js

--- www/js/services/services-auth.js
+++ www/js/services/services-auth.js
@@ -3,13 +3,13 @@ var authServices = angular.module('authServices', ['ngResource']);
 var signupUrl = '/api/users';
 var loginUrl = '/api-token-auth/';
 
-authServices.service('signupService', [ '$resource', function ($resource) {
+authServices.service('signupService', [ '$resource', '$rootScope', function ($resource, $rootScope) {
   return $resource($rootScope.version + $rootScope.baseUrl + signupUrl +':id/?format=json', {},{
     create: { method: 'POST' }
   })
 }])
 
-authServices.service('loginService', [ '$resource', function ($resource) {
+authServices.service('loginService', [ '$resource', '$rootScope', function ($resource, $rootScope) {
   return $resource($rootScope.version + $rootScope.baseUrl + loginUrl +':id/?format=json', {},{
     create: { method: 'POST' }
   })
